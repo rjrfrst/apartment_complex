a9127fff3a0befe777018e811b7b4398eaae230e
rjrfrst <rjrfrst@gmail.com>
2024-04-15 17:29:58 +0100

    dummy commit, inside a model. No db created yet

---
+++ ApartmentComplexServer/src/main/java/com/project/ApartmentComplexServer/models/UserNormal.java
@@ -0,0 +1,9 @@
+package com.project.ApartmentComplexServer.models;
+
+public class UserNormal {
+
+    public static void main(String[] args) {
+        System.out.println("in User normal");
+    }
+
+} //last
