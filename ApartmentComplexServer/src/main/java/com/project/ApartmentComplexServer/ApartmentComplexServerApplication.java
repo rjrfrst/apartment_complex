522e53cd0f1430d10660d4c53918221ead8a1411
rjrfrst <rjrfrst@gmail.com>
2024-04-15 17:26:03 +0100

    initial commit, both client and server folder added. Create db next and client(react) in the future

---
+++ ApartmentComplexServer/src/main/java/com/project/ApartmentComplexServer/ApartmentComplexServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.ApartmentComplexServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentComplexServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentComplexServerApplication.class, args);
+	}
+
+}
---
+++ ApartmentComplexServer/src/test/java/com/project/ApartmentComplexServer/ApartmentComplexServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ApartmentComplexServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentComplexServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
